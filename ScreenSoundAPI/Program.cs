16df44ccbde7f2d9ff35ab58ed4eb5e5bcc24f5f
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-17 15:23:07 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,11 @@
+﻿using (HttpClient client = new())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(response);
+    } catch (Exception error)
+    {
+        Console.WriteLine($"Error Message: {error.Message}");
+    }
+}
