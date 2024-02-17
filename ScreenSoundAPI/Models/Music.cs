d0fb49fcfad15cbe0e2135b85d98591a15b03261
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-17 15:32:03 -0300

    feat: Music class

---
+++ ScreenSoundAPI/Models/Music.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSoundAPI.Models;
+
+internal class Music
+{
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duration { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+
+    public void ShowMusicDetails()
+    {
+        Console.WriteLine($"Music: {this.Name}");
+        Console.WriteLine($"Artist: {this.Artist}");
+        Console.WriteLine($"Duration: {this.Duration / 1000}");
+        Console.WriteLine($"Genre: {this.Genre}");
+    }
+
+}
